75e6778ffe361a8c76454a74ce63be066a82c88c
jorgeyro <jorgeyro@gmail.com>
2026-06-06 12:34:41 -0300

    hotd-guide: arbol sin actores, sin Corlys/Rhaenys

--- hotd-guide/js/app.js
+++ hotd-guide/js/app.js
@@ -543,14 +543,14 @@
     // Spouses row
     const alicent = data.characters.find(c => c.id === 'alicent');
     html += `<div class="ft-spouses-wrap">
-      <div class="ft-spouse" data-char="alicent">
-        <div class="ft-avatar-sm">${createAvatarHTML(alicent)}</div>
-        <div class="ft-name-sm">Alicent Hightower</div>
-      </div>
-      <div class="ft-spouse-divider">⚭</div>
       <div class="ft-spouse">
         <div class="ft-avatar-sm" style="filter:grayscale(1);opacity:0.6">${createAvatarHTML({name:'Aemma Arryn',actor:'',image:''})}</div>
         <div class="ft-name-sm ft-dead">Aemma Arryn †</div>
+      </div>
+      <div class="ft-spouse-divider">⚭</div>
+      <div class="ft-spouse" data-char="alicent">
+        <div class="ft-avatar-sm">${createAvatarHTML({name:'Alicent Hightower',actor:'',image:''})}</div>
+        <div class="ft-name-sm">Alicent Hightower</div>
       </div>
     </div>`;
 
